d288bd761e727c3672a87422ef71f4be862d1c9a
SkyXploit <89053641+SkyXploit@users.noreply.github.com>
2021-08-17 09:06:17 +0700

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,39 @@
+import os
+
+os.system("cls")
+
+print("masukan pin anda")
+password=str(input("pin: "))
+if ( password=="1401041"):
+
+ print("SELAMAT DATANG DI ATM IRFAN")
+print("pilih option:")
+print("1. cek uang saya")
+print("2. ambil uang saya")
+print("3. tabung uang saya")
+option=int(input("silahkan pilih option:"))
+if option==1:
+    print("uang kamu berjumlah Rp.200.000")
+elif option==2:
+    print("uang kamu berjumlah Rp.200.000, mau ambil berapa?")
+    print("1. 100.000")
+    print("2. 200.000")
+    uang_kamu=200000
+    option2=int(input("option :"))
+    if option2==1:
+        hasil=uang_kamu-100000
+        print("uang kamu sekarang berjumlah :",hasil)
+    elif option2==2:
+            hasil2=uang_kamu-200000
+            print("sekarang uang kamu berjumlah :",hasil2)
+    else:
+            print("keyword anda salah!")
+elif option==3:
+    print("uang berjumlah Rp.200.000 mau tabung berapa?")
+    uang_saya=200000
+    option3=int(input("masukan jumlah uang ;"))
+    hasil3=uang_saya+option3
+    print("jumlah uang kamu sekarang adalah ",hasil3)
+else:
+    print("keyword anda salah!, mohon coba lagi")    
+
